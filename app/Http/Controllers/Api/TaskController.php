94bd68cf005a4243adfb48281e97c9c3d458dfb7
DanielaEsposito <esposito.daniela9817@gmail.com>
2025-05-09 11:09:55 +0200

    add filter logic to index function

--- app/Http/Controllers/Api/TaskController.php
+++ app/Http/Controllers/Api/TaskController.php
@@ -14,11 +14,25 @@ class TaskController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
+        $category = $request->input('category');
+        $priority = $request->input('priority');
+        //recupero l'utente autenticato
         $user = Auth::user();
-        $tasks = $user->tasks;
-        $tasks->load(['category', 'priority']);
+        //alla variabile $query assegno la query per recuperare i task dell'utente
+        $query = Task::where('user_id', $user->id);
+        //se la variabile $category non è vuota, aggiungo alla query il filtro per categoria
+        if ($category) {
+            $query->where('category_id', $category);
+        }
+        //se la variabile $priority non è vuota, aggiungo alla query il filtro per priorità
+        if ($priority) {
+            $query->where('priority_id', $priority);
+        }
+
+        $tasks = $query->with('category', 'priority')->get();
+
         return response()->json($tasks);
     }
 
